2c71817c0b4a6bba210b1c76f4339a5971fa223a
Idan Maimon <idanmaimon381891@gmail.com>
2023-01-18 23:34:32 +0200

    appHeader

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -4,29 +4,24 @@ import { NavLink, withRouter } from "react-router-dom";
 class _AppHeader extends Component {
 
     state = {
-        isNavOn: false,
         navRoutes: ['home', 'contact', 'statistic']
     }
-    onChangeNav = (boolen) => {
-        this.setState({ isNavOn: boolen })
-    }
+
     render() {
-        const { isNavOn, navRoutes } = this.state
+        const { navRoutes } = this.state
         return (
-            <section onMouseOver={() => this.onChangeNav(true)} onMouseLeave={() => this.onChangeNav(false)} className='header-cmp'>
+            <section className='header-cmp'  >
                 <div className='logo-cont'>
                     <span className='nav-icon'>&#x2630;</span>
                     <div>
                         Bitcoin<span className='blue-color'>A</span>pp
                     </div>
                 </div>
-                {isNavOn &&
-                    <nav>
-                        {navRoutes.map(routeName =>
-                            <NavLink className='nav-link' exact to={'/' + (routeName === 'home' ? '' : routeName)} key={routeName}>{routeName}</NavLink>
-                        )}
-                    </nav>
-                }
+                <nav>
+                    {navRoutes.map(routeName =>
+                        <NavLink className='nav-link' exact to={'/' + (routeName === 'home' ? '' : routeName)} key={routeName}>{routeName}</NavLink>
+                    )}
+                </nav>
             </section>
         )
     }
