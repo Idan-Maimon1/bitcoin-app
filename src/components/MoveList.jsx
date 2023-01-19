7cb79093b700ad771ad1f8f05720e03ad711d2fb
Idan Maimon <idanmaimon381891@gmail.com>
2023-01-20 00:44:55 +0200

    move list ui

--- src/components/MoveList.jsx
+++ src/components/MoveList.jsx
@@ -1,6 +1,7 @@
 import { Component } from 'react'
 import { connect } from 'react-redux'
 import { transferCoins } from '../store/actions/userActions'
+import transferImg from '../assets/imgs/transfer-img.png'
 
 class _MoveList extends Component {
 
@@ -11,28 +12,41 @@ class _MoveList extends Component {
     }
 
     formatDate(timeStamp) {
-        const day = new Date(timeStamp).getDate()
-        const month = (new Date(timeStamp).getMonth() + 1)
         const hour = new Date(timeStamp).getHours()
         const minutes = new Date(timeStamp).getMinutes()
-        return day + '/' + month + ' ' + hour + ':' + minutes
+        const day = new Date(timeStamp).getDate()
+        const month = (new Date(timeStamp).getMonth() + 1)
+        const year = (new Date(timeStamp).getYear() + '').slice(-2)
+        return hour + ':' + minutes + ' ' + day + '/' + month + '/' + year
     }
 
     render() {
         const { loggedInUser, contact } = this.props
         if (!loggedInUser) return
-        const moves = contact ? this.filterMoves(loggedInUser.moves, contact) : loggedInUser.moves
+        const moves = contact ? this.filterMoves(loggedInUser.moves, contact) : loggedInUser.moves.slice(-6)
         if (!moves || !moves.length) return
         return (
             <section className='move-list'>
-                <h1>MoveList</h1>
-                {moves.map(move =>
-                    <section key={move.at}>
-                        <p>To: {move.to}</p>
-                        <p>Amount: {move.amount}</p>
-                        <p>At: {this.formatDate(move.at)}</p>
-                    </section>
-                )}
+                <div className='move-list-header'>
+                    <h1><span className="blue-color">
+                        Recent</span> Moves</h1>
+                    <p>In this section you can see your last 6 approved moves</p>
+                </div>
+                <div className='transfers-container'>
+                    {moves.map(move =>
+                        <section key={move.at}>
+                            <div className='transfer-block-top'>
+                                <img src={transferImg} alt="" />
+                                <p>{move.to}</p>
+                            </div>
+                            <div>
+                                <p>You successfully transferd {move.amount} coins</p>
+                                <p>To {move.to}</p>
+                            </div>
+                            <p className='transfer-date'> {this.formatDate(move.at)}</p>
+                        </section>
+                    )}
+                </div>
             </section>
         )
     }
