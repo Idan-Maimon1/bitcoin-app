1bcd804e5cf8e57cd80d11e3b83e31d8701edd84
Idan Maimon <idanmaimon381891@gmail.com>
2023-01-22 22:40:43 +0200

    general improvements

--- src/components/MoveList.jsx
+++ src/components/MoveList.jsx
@@ -19,7 +19,7 @@ class _MoveList extends Component {
     formatDate(timeStamp) {
         const date = new Date(timeStamp)
         const hours = date.getHours() < 10 ? '0' + date.getHours() : date.getHours()
-        const minutes = date.getMinutes()
+        const minutes = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()
         const day = date.getDate()
         const month = (date.getMonth() + 1)
         const year = (date.getYear() + '').slice(-2)
--- src/services/userService.js
+++ src/services/userService.js
@@ -7,7 +7,6 @@ export const userService = {
     setUser,
     addMove
 }
-
 const DemoUser = {
     loggedInUser: {
         name: "Demo User",
@@ -19,6 +18,8 @@ const DemoUser = {
             { toId: "5a5664025f6ae9aa24a99fde", to: "Hallie Mclean", at: 1674242000000, amount: 60 },
             { toId: "5a56640252d6acddd183d319", to: "Parsons Norris", at: 1674590000000, amount: 100 },
             { toId: "5a56640252d6acddd183d319", to: "Parsons Norris", at: 1674605606493, amount: 25 },
+            { toId: "5a566402abb3146207bc4ec5", to: "Floyd Rutledge", at: 1674419706808, amount: 100 },
+            { toId: "90896b13a81d08ee64028c0a", to: "Perlin Perlino", at: 1674419856915, amount: 30 },
             { toId: "495af35664cf002ed1b47b4d", to: "Joey Geller", at: 1674680612774, amount: 32 }]
     }
 }
